435c353223b09b273ca2472516029bfbedf48ef5
MihirMistry2 <mihirmistry82@gmail.com>
2025-12-31 19:53:11 +0530

    chore: adjust Vite config for library output

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'ReactCodeEditor',
-            fileName: (format) => `react-code-editor.${format}.js`,
+            fileName: (format) => `index.${format}.js`,
         },
         rollupOptions: {
             external: ['react', 'react-dom'],
