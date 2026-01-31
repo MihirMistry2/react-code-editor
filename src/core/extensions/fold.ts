81fb6198a01b9577532f57c63fc2fb12b771b3c6
MihirMistry2 <mihirmistry82@gmail.com>
2026-01-31 20:44:08 +0530

    fix(fold): recursively fold nested json blocks

--- src/core/extensions/fold.ts
+++ src/core/extensions/fold.ts
@@ -1,10 +1,31 @@
 import type { EditorView } from '@codemirror/view';
-import { foldAll, unfoldAll } from '@codemirror/language';
+import type { StateEffect } from '@codemirror/state';
+import {
+    syntaxTree,
+    foldable,
+    foldEffect,
+    unfoldAll,
+} from '@codemirror/language';
 
 export const foldAllCode = (view: EditorView | null | undefined) => {
     if (!view) return false;
 
-    return foldAll(view);
+    const effects: StateEffect<unknown>[] = [];
+
+    syntaxTree(view.state).iterate({
+        enter(node) {
+            const range = foldable(view.state, node.from, node.to);
+
+            if (range) {
+                effects.push(foldEffect.of(range));
+            }
+        },
+    });
+
+    if (!effects.length) return false;
+
+    view.dispatch({ effects });
+    return true;
 };
 
 export const unfoldAllCode = (view: EditorView | null | undefined) => {
