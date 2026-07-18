9f9d46cbf10d9f4f52ef40dfac947e187c92baae
MihirMistry2 <mihirmistry82@gmail.com>
2026-07-18 11:34:10 +0530

    refactor(core): move language plugins into plugins directory

--- src/core/languages/registry.ts
+++ src/core/languages/registry.ts
@@ -1,19 +1,13 @@
 import type { EditorLanguage, LanguagePlugin } from '../../types';
 
-const registry = new Map<EditorLanguage, LanguagePlugin<EditorLanguage>>();
+import { jsonPlugin } from './plugins';
 
-export function registerLanguage<Language extends EditorLanguage>(
-    plugin: LanguagePlugin<Language>,
-) {
-    if (registry.has(plugin.id)) {
-        throw new Error(`Language plugin '${plugin.id}' already registered`);
-    }
-
-    registry.set(plugin.id, plugin);
-}
+const registry: Record<string, LanguagePlugin<any>> = {
+    json: jsonPlugin,
+};
 
 export function getLanguagePlugin<Language extends EditorLanguage>(
     language: Language,
 ): LanguagePlugin<Language> | undefined {
-    return registry.get(language) as LanguagePlugin<Language> | undefined;
+    return registry[language] as LanguagePlugin<Language> | undefined;
 }
